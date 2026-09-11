7a189ae6793c84878cb771efa354782796b3e053
Matsushita1907 <theus.matsu@gmail.com>
2026-09-11 19:00:12 -0300

    iniciando projeto de sistema de rpg

---
+++ main.py
@@ -0,0 +1,8 @@
+print ("-----RPG BATTLEGROUND-----")
+nome = input("Digite o nome do seu personagem: ")
+print ("Seja bem vindo,", nome)
+print ("Escolha a sua classe !")
+print("1 Guerreiro")
+print("2 Mago")
+print("3 Arqueiro")
+
