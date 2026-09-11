21ed94e9be739e68dd4ad12e6812c9577a0f466d
Matsushita1907 <theus.matsu@gmail.com>
2026-09-11 20:57:27 -0300

    dando continuação na classe "Player"

--- player.py
+++ player.py
@@ -8,3 +8,20 @@ class Player:
         self.ataque = ataque
         self.defesa = defesa
 
+    def mostrar_ficha(self):
+        print()
+        print("PERSONAGEM CRIADO")
+        print("Nome:", self.nome)
+        print("Classe:", self.classe)
+        print("HP:", self.hp)
+        print("Ataque:", self.ataque)
+        print("Defesa:", self.defesa)
+
+    def receber_dano(self, dano):
+        self.hp -= dano
+
+        if self.hp < 0:
+            self.hp = 0
+
+    def esta_vivo(self):
+        return self.hp > 0
