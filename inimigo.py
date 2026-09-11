8b0e1e3501ae18c4f369a0fe3ea2535c71498ab2
Matsushita1907 <theus.matsu@gmail.com>
2026-09-11 20:58:18 -0300

    criando a classe "inimigo"

---
+++ inimigo.py
@@ -0,0 +1,8 @@
+class Enemy:
+
+    def __init__(self, nome, hp, ataque, defesa):
+        self.nome = nome
+        self.hp = hp
+        self.hp_max = hp
+        self.ataque = ataque
+        self.defesa = defesa
