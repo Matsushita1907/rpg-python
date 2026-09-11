bda88533136245b138c04eff109ea90bfaf66386
Matsushita1907 <theus.matsu@gmail.com>
2026-09-11 20:56:47 -0300

    criando classe "Player"

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+
+    def __init__(self, nome, classe, hp, ataque, defesa):
+        self.nome = nome
+        self.classe = classe
+        self.hp = hp
+        self.hp_max = hp
+        self.ataque = ataque
+        self.defesa = defesa
+
