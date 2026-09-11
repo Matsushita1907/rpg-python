ecfb56f13550b923b72101159e5d47e8bf450ad0
Matsushita1907 <theus.matsu@gmail.com>
2026-09-11 20:58:39 -0300

    continuação na classe "inimigo"

--- inimigo.py
+++ inimigo.py
@@ -6,3 +6,20 @@ class Enemy:
         self.hp_max = hp
         self.ataque = ataque
         self.defesa = defesa
+
+    def receber_dano(self, dano):
+        self.hp -= dano
+
+        if self.hp < 0:
+            self.hp = 0
+
+    def esta_vivo(self):
+        return self.hp > 0
+
+    def mostrar_status(self):
+        print()
+        print("INIMIGO")
+        print("Nome:", self.nome)
+        print("HP:", self.hp)
+        print("Ataque:", self.ataque)
+        print("Defesa:", self.defesa)
