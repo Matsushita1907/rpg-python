756a98ee4ff95015b877c890cf54249be78701e8
Matsushita1907 <theus.matsu@gmail.com>
2026-09-11 21:07:56 -0300

    criação das opções de batalha na classe "batalha"

--- batalha.py
+++ batalha.py
@@ -5,3 +5,26 @@ def calcular_dano(ataque, defesa):
         dano = 0
 
     return dano
+
+def batalha(jogador, inimigo):
+
+    print()
+    print("INIMIGO ENCONTRADO")
+
+    inimigo.mostrar_status()
+
+    while jogador.esta_vivo() and inimigo.esta_vivo():
+
+        print()
+        print("BATALHA")
+
+        print("Seu HP:", jogador.hp)
+        print("HP do", inimigo.nome + ":", inimigo.hp)
+
+        print()
+        print("[1] Ataque normal")
+        print("[2] Ataque forte")
+        print("[3] Defender")
+        print("[4] Fugir")
+
+        acao = input("Escolha sua ação: ")
